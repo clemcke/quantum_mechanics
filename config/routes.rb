24d4676b7caf08f192a501b475ef29a4db0f4875
Chris Lemcke <sixtimesnine@gmail.com>
2012-07-11 14:45:33 -0700

    forgot to add songs route

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ QuantumMechanics::Application.routes.draw do
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
+  resources :songs
+
   # Sample of regular route:
   #   match 'products/:id' => 'catalog#view'
   # Keep in mind you can assign values other than :controller and :action
