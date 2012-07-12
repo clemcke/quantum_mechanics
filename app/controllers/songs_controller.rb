aa3650fb2a821350019811c5e2a84b664d04ec1d
Chris Lemcke <sixtimesnine@gmail.com>
2012-07-11 23:28:40 -0700

    Implemented some nice styling for the home page

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,55 +1,27 @@
 class SongsController < ApplicationController
-  # GET /songs
-  # GET /songs.json
   def index
-    @songs = Song.all
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json { render json: @songs }
-    end
+    @jams = Song.all.group_by(&:recorded_on)
   end
 
-  # GET /songs/1
-  # GET /songs/1.json
   def show
     @song = Song.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @song }
-    end
   end
 
-  # GET /songs/new
-  # GET /songs/new.json
   def new
     @song = Song.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @song }
-    end
   end
 
-  # GET /songs/1/edit
   def edit
     @song = Song.find(params[:id])
   end
 
-  # POST /songs
-  # POST /songs.json
   def create
     @song = Song.new(params[:song])
 
-    respond_to do |format|
-      if @song.save
-        format.html { redirect_to @song, notice: 'Song was successfully created.' }
-        format.json { render json: @song, status: :created, location: @song }
-      else
-        format.html { render action: "new" }
-        format.json { render json: @song.errors, status: :unprocessable_entity }
-      end
+    if @song.save
+      redirect_to @song, notice: 'Song was successfully created.'
+    else
+      render action: "new"
     end
   end
 
